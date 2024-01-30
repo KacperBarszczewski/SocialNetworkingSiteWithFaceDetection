ba12fcbf6491703a6f25e266de20c498b5ad21c2
KacperB <kacpero.barszczewski@gmail.com>
2024-01-30 01:31:27 +0100

    add pages and config routes

--- frontend-Angular/src/app/app.routes.ts
+++ frontend-Angular/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'unlogged',
+    loadComponent: () =>
+      import('./pages/unlogged/unlogged.component').then(
+        (m) => m.UnloggedComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'unlogged',
+  },
+];
---
+++ frontend-Angular/src/app/pages/unlogged/unlogged.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unlogged',
+  standalone: true,
+  imports: [],
+  templateUrl: './unlogged.component.html',
+  styleUrl: './unlogged.component.css'
+})
+export class UnloggedComponent {
+
+}
