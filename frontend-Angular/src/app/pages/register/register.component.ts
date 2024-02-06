8c0b67cca01517c2b07541905b91a130359b2ed0
KacperB <kacpero.barszczewski@gmail.com>
2024-02-06 16:28:31 +0100

    register page

--- frontend-Angular/src/app/pages/register/register.component.ts
+++ frontend-Angular/src/app/pages/register/register.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  loginError: string = '';
+  showPassword: boolean = false;
+  showRepPassword: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    repPassword: ['', [Validators.required,Validators.minLength(3)]],
+  });
+
+  onSubmit() {
+    const { email, password } = this.form.getRawValue();
+
+    this.authService.login(email, password).subscribe({
+      next: (res) => {
+        this.router.navigateByUrl('/home');
+      },
+      error: (err) => {
+        this.loginError = err.error.message;
+      },
+    });
+  }
 }
