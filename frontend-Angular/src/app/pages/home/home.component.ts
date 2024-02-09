e9ffa158254c5e18c775ab67090442ef396638fd
KacperB <kacpero.barszczewski@gmail.com>
2024-02-10 00:45:06 +0100

    style home page

--- frontend-Angular/src/app/pages/home/home.component.ts
+++ frontend-Angular/src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  name = this.authService.getFullCurrentUserData().user.name;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.signOut();
+    this.router.navigateByUrl('/');
+  }
 }
