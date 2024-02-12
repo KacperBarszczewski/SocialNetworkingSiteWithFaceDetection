945a434c897736e739e18d2a802d849e47e2d224
KacperB <kacpero.barszczewski@gmail.com>
2024-02-12 20:56:02 +0100

    add form to create post

--- frontend-Angular/src/app/pages/home/home.component.ts
+++ frontend-Angular/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,28 @@ import { HeaderComponent } from '../../components/header/header.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  imageSrc: string | ArrayBuffer | null = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  form = this.fb.nonNullable.group({
+    image: [''],
+    opis: ['', [Validators.required]],
+  });
+
+  onImageChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files[0]) {
+      const file = inputElement.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64Image = reader.result as string;
+        this.form.patchValue({ image: base64Image });
+        
+        this.imageSrc=base64Image;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
