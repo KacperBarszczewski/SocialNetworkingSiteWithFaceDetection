601d44ec5f16fd615a99b117f6cf3a0c8cc6b869
KacperB <kacpero.barszczewski@gmail.com>
2024-02-14 16:17:44 +0100

    Add image format validation in upload_file function

--- backend-python/app.py
+++ backend-python/app.py
@@ -4,6 +4,7 @@ import cv2
 from mtcnn import MTCNN
 import numpy as np
 import base64
+import imghdr
 
 app = Flask(__name__)
 CORS(app, origins="*")
@@ -11,24 +12,23 @@ CORS(app, origins="*")
 def detect_faces(image):
     detector = MTCNN()
     faces = detector.detect_faces(image)
-
     return faces
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'image' not in request.json:
+    image_data = request.get_json().get('image')
+    
+    if not image_data:
         return jsonify({'error': 'Brak zdjęcia'}), 400
 
-    image_data = request.json['image']
-    if not image_data.startswith('data:image'):
-        return jsonify({'error': 'Zły format zdjęcia'}), 400
-
-    # Usunięcie nagłówka "data:image/png;base64,"
     base64_img = image_data.split('base64,')[1]
 
-    # Dekodowanie obrazu z base64
     img_bytes = base64.b64decode(base64_img)
     img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+
+    image_format = imghdr.what('', h=img_bytes)
+    if not image_format or image_format not in ['jpeg', 'png', 'gif']:
+        return jsonify({'error': 'Zły format zdjęcia'}), 400
 
     try:
         faces = detect_faces(img)
