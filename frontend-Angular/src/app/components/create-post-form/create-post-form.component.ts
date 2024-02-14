142f802d05202d9d1e6864334c4d2b1b3c5fd7a8
KacperB <kacpero.barszczewski@gmail.com>
2024-02-14 20:59:51 +0100

    Add create post form component

---
+++ frontend-Angular/src/app/components/create-post-form/create-post-form.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PostService } from '../../services/post/post.service';
+
+@Component({
+  selector: 'home-create-post-form',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './create-post-form.component.html',
+  styleUrl: './create-post-form.component.css',
+})
+export class CreatePostFormComponent {
+  imageSrc: string = '';
+  postError: string = '';
+
+  constructor(private fb: FormBuilder, private postService: PostService) {}
+
+  form = this.fb.nonNullable.group({
+    image: [''],
+    description: ['', [Validators.required]],
+  });
+
+  onImageChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files[0]) {
+      this.postError = '';
+      const file = inputElement.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64Image = reader.result as string;
+        this.imageSrc = base64Image;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const { description } = this.form.getRawValue();
+
+    this.postService.postPost(this.imageSrc, description).subscribe({
+      next: (res) => {
+        this.form.reset();
+        this.imageSrc = '';
+        this.postError = '';
+        this.postService.getPosts().subscribe();
+      },
+      error: (err) => {
+        this.postError = err.error.message;
+      },
+    });
+  }
+}
--- frontend-Angular/src/app/pages/home/home.component.ts
+++ frontend-Angular/src/app/pages/home/home.component.ts
@@ -1,67 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import {
-  FormBuilder,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { PostService } from '../../services/post/post.service';
-import { CommonModule } from '@angular/common';
 import { PostsComponent } from '../../components/posts/posts.component';
+import { CreatePostFormComponent } from '../../components/create-post-form/create-post-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    PostsComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    CommonModule,
-  ],
+  imports: [HeaderComponent, CreatePostFormComponent, PostsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  imageSrc: string = '';
-  postError: string = '';
-
-  constructor(private fb: FormBuilder, private postService: PostService) {}
-
-  form = this.fb.nonNullable.group({
-    image: [''],
-    description: ['', [Validators.required]],
-  });
-
-  onImageChange(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement.files && inputElement.files[0]) {
-      this.postError = '';
-      const file = inputElement.files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        const base64Image = reader.result as string;
-        this.imageSrc = base64Image;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-  onSubmit(event: Event) {
-    event.preventDefault();
-    const { description } = this.form.getRawValue();
-
-    this.postService.postPost(this.imageSrc, description).subscribe({
-      next: (res) => {
-        this.form.reset();
-        this.imageSrc = '';
-        this.postError = '';
-        this.postService.getPosts().subscribe();
-      },
-      error: (err) => {
-        this.postError = err.error.message;
-      },
-    });
-  }
-}
+export class HomeComponent {}
